b76abad9d6c6ec4598c8809ba54bff37d336f977
Johannes Raunio <johannes.raunio@gmail.com>
2014-02-28 15:29:23 +0000

    Added project page and a handelbar template for projects page.

--- js/main.js
+++ js/main.js
@@ -1,102 +1,61 @@
-var app = {
+(function () {
+  var init = function () {
+    // Make sure the dialog is in the DOM
+    if (document.querySelector('#my-dialog')) {
+      var dialog = new fries.Dialog({
+        selector: '#my-dialog',
+        callbackOk: function () {
+          var toast = new fries.Toast({
+            content: "Pressed OK",
+            duration: fries.Toast.duration.SHORT
+          });
 
-	initialize : function() {
-		var self = this;
-		this.detailsURL = /^#employees\/(\d{1,})/;
-		self.registerEvents();
-		this.store = new MemoryStore(function() {
-			self.route();
-		});
-	},
+          this.hide(); // this refers to the dialog
+        },
+        callbackCancel: function () {
+          var toast = new fries.Toast({
+            content: "Pressed Cancel",
+            duration: fries.Toast.duration.SHORT
+          });
 
-	showAlert : function(message, title) {
-		if (navigator.notification) {
-			navigator.notification.alert(message, null, title, 'OK');
-		} else {
-			alert( title ? (title + ": " + message) : message);
-		}
-	},
+          this.hide(); // this refers to the dialog
+        }
+      });
 
-	registerEvents : function() {
-		var self = this;
-		$(window).on('hashchange', $.proxy(this.route, this));
-		// Check of browser supports touch events...
-		if (document.documentElement.hasOwnProperty('ontouchstart')) {
-			// ... if yes: register touch event listener to change the "selected" state of the item
-			$('body').on('touchstart', 'a', function(event) {
-				$(event.target).addClass('tappable-active');
-			});
-			$('body').on('touchend', 'a', function(event) {
-				$(event.target).removeClass('tappable-active');
-			});
-		} else {
-			// ... if not: register mouse events instead
-			$('body').on('mousedown', 'a', function(event) {
-				$(event.target).addClass('tappable-active');
-			});
-			$('body').on('mouseup', 'a', function(event) {
-				$(event.target).removeClass('tappable-active');
-			});
-		}
-	},
+      document.querySelector('#open-dialog').addEventListener('touchend', function (e) {
+        e.preventDefault();
+        dialog.show();
+      }, false);
+    }
 
-	route : function() {
-		var self = this;
-		var hash = window.location.hash;
-		if (!hash) {
-			if (this.homePage) {
-				this.slidePage(this.homePage);
-			} else {
-				this.homePage = new HomeView(this.store).render();
-				this.slidePage(this.homePage);
-			}
-			return;
-		}
-		var match = hash.match(this.detailsURL);
-		if (match) {
-			this.store.findById(Number(match[1]), function(employee) {
-				self.slidePage(new EmployeeView(employee).render());
-			});
-		}
-	},
+    document.querySelector('#show-toast').addEventListener('touchend', function () {
+      var toast = new fries.Toast({ content: "Hi, I'm a Toast notification." });
 
-	slidePage : function(page) {
+    }, false);
+  };
 
-		var currentPageDest, self = this;
+  window.addEventListener('push', init, false);
+  window.addEventListener('popstate', init, false);
 
-		// If there is no current page (app just started) -> No transition: Position new page in the view port
-		if (!this.currentPage) {
-			$(page.el).attr('class', 'page stage-center').attr('id', 'homePage');
-			$('body').append(page.el);
-			this.currentPage = page;	
-			return;
-		}
+  var detect = function () {
+    if( navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
 
-		// Cleaning up: remove old pages that were moved out of the viewport
-		$('.stage-right, .stage-left').not('#homePage').remove();
-
-		if (page === app.homePage) {
-			// Always apply a Back transition (slide from left) when we go back to the search page
-			$(page.el).attr('class', 'page stage-left');
-			currentPageDest = "stage-right";
-		} else {
-			// Forward transition (slide from right)
-			$(page.el).attr('class', 'page stage-right');
-			currentPageDest = "stage-left";
-		}
-
-		$('body').append(page.el);
-
-		// Wait until the new page has been added to the DOM...
-		setTimeout(function() {
-			// Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
-			$(self.currentPage.el).attr('class', 'page transition ' + currentPageDest);
-			// Slide in the new page
-			$(page.el).attr('class', 'page stage-center transition');
-			self.currentPage = page;
-		});
-
-	},
-};
-
-app.initialize();
+  // To enable touch events on desktop
+  // Remove this when building in PhoneGap
+  if (detect) {
+    window.onload = new FingerBlast('body');
+  }
+} ());
